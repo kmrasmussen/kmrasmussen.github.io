f0065d101135324cffb6e9fb9daf808d2ba43a0b
Kasper Rasmussen <obscur@tuta.io>
2019-12-23 13:26:30 +0100

    p5 work1b

--- sketch.js
+++ sketch.js
@@ -7,10 +7,10 @@ let stopTime = 1500
 let canvWidth = window.innerWidth
 let canvHeight = window.innerHeight
 
-let circleRadius = canvHeight / 3
+let circleRadius = (canvHeight / 3)
 let circleCenterX = canvWidth / 2
 let circleCenterY = canvHeight / 2
-let n_movers = 3
+let n_movers = 4
 let moverStartPos = [circleCenterX, circleCenterY]
 let movers = []
 let strokeColors = []
@@ -20,9 +20,17 @@ for(i = 0; i < n_movers; i++) {
     movers.push(moverStartPos)
     strokeColors.push(strokeColorInitial)
 }
-console.log("Initialised strokeColors")
-console.log(strokeColors)
-console.log(movers)
+
+function generateColorVariant(original) {
+    hue_noise = randomGaussian(0, 20)
+    new_hue = (original[0] + hue_noise) % 100
+    saturation_noise = abs(randomGaussian(0, 30))
+    new_saturation = original[1] - saturation_noise
+    brightness_noise = abs(randomGaussian(0, 30))
+    new_brightness = original[2] - brightness_noise
+    return [new_hue, new_saturation, new_brightness]
+}
+
 
 function k_point(vec) {
     point(vec[0], vec[1])
@@ -35,29 +43,30 @@ function k_line(vec1, vec2) {
 function changeStroke(i) {
     strokeWeight(randomGaussian(4,7))
     stroke(strokeColors[i][0], strokeColors[i][1], strokeColors[i][2])
-    console.log("Current stroke:")
-    console.log(strokeColors[i])
 }
 
 function setup() {
+    colorMode(HSB, 100)
     createCanvas(canvWidth, canvHeight)
-    background(255,255,255)
-    stroke(255,255,255)
-    circle(circleCenterX, circleCenterY, 2 * circleRadius)
-    stroke(0,0,0)
-    strokeWeight(3)
-    console.log("Color randomness, initial hue: " + hue)
+    background_hue = hue + randomGaussian(0,20)
+    background_saturation = abs(randomGaussian(0,20)) //100 - abs(randomGaussian(0, 30))
+    background_brightness = 100 - abs(randomGaussian(0, 20)) //100 - abs(randomGaussian(50, 30))
+    background(background_hue, background_saturation, background_brightness)
+
     newColors = []
     for(i = 0; i < strokeColors.length; i++) {
-        hue_noise = randomGaussian(0, 50)
-        new_hue = (strokeColors[i][0] + hue_noise) % 100
-        newColors.push([new_hue, 100, 100])
+        newColors.push(generateColorVariant(strokeColorInitial))
     }
-    console.log("After noise ")
     strokeColors = newColors
-    console.log(strokeColors)
-    console.log(newColors)
-    colorMode(HSB, 100)
+
+    textSize((circleRadius / 3) * randomGaussian(1, 0.1));
+    textColor = generateColorVariant(strokeColorInitial) 
+    fill(hue, 100, 100);
+    courierFont = loadFont('COURIER.TTF');
+    textFont("Courier");
+    rotate(HALF_PI);
+    text('CLOSURE', 0, 0) //circleCenterX * 0.7, canvHeight / 2 - (1.25 * circleRadius));
+    
     //noLoop()
 };
 
@@ -77,9 +86,6 @@ function draw() {
             deltay = randomGaussian(0,5)
             newPoint = [mover[0] + deltax, mover[1] + deltay]
         }
-        console.log("Calling change stroke " + i)
-        console.log("All colors:")
-        console.log(strokeColors)
         changeStroke(i)
         k_point(newPoint)
         //k_line(mover, newPoint)
